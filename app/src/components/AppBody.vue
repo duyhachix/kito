2cc86e3c278e13657b9f7217ac21ea29b7923f66
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-12-26 01:00:53 +0700

    responsive about section

--- app/src/components/AppBody.vue
+++ app/src/components/AppBody.vue
@@ -298,4 +298,410 @@ export default {
 		}
 	}
 }
+@media screen and (max-width: 1700px) {
+	.body {
+		padding: 0 255px;
+		width: 100%;
+		height: 100%;
+		.section {
+			margin-bottom: 100px;
+		}
+		/* &__title {
+			margin-top: 100px;
+			text-align: left;
+			width: fit-content;
+			p {
+				font-family: Sansita;
+				font-size: 54px;
+				font-style: normal;
+				font-weight: 700;
+				margin-bottom: 10px;
+			}
+			&--option {
+				display: flex;
+				align-items: center;
+				h6 {
+					font-family: Open Sans;
+					font-size: 15px;
+					font-style: normal;
+					line-height: 26px;
+				}
+				.vertical-line {
+					width: 2px;
+					height: 50%;
+					background-color: $text-color;
+					display: flex;
+					margin: 0 15px;
+				}
+			}
+		}
+		&__cta {
+			margin-top: 70px;
+			width: 100%;
+			background: #e5f8fd;
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			&--left {
+				padding: 100px 70px;
+				text-align: left;
+				height: fit-content;
+				.about {
+					border-radius: 5px;
+					background: #e6e6ef;
+					width: fit-content;
+					padding: 7px 15px;
+					color: #ec80a0;
+				}
+				.quote {
+					font-family: Sansita;
+					font-size: 54px;
+					font-style: normal;
+					font-weight: 700;
+					line-height: 70px;
+					margin-bottom: 40px;
+				}
+				.contact {
+					padding: 17px 40px;
+					background-color: #ec80a0;
+					text-align: center;
+					width: fit-content;
+					border-radius: 10px;
+				}
+			}
+			&--right {
+				width: 100%;
+				height: 100%;
+				display: grid;
+				.video {
+					align-self: center;
+					video {
+						width: 100%;
+						height: 100%;
+					}
+				}
+			}
+		}
+		&__proud {
+			&--title {
+				p {
+					font-family: Sansita;
+					font-size: 54px;
+					font-style: normal;
+					font-weight: 700;
+					line-height: 70px;
+					margin-bottom: 50px;
+				}
+			}
+			&--cardGroup {
+				width: 100%;
+				height: 100%;
+				display: grid;
+				grid-template-columns: 1fr 1fr 1fr 1fr;
+				column-gap: 30px;
+				.card {
+					cursor: pointer;
+					border-radius: 10px;
+					box-shadow: 0px 4px 30px 0px rgba(10, 30, 35, 0.1);
+					h2 {
+						font-weight: 700;
+						line-height: 55px;
+						font-family: Sansita;
+						font-size: 42px;
+						color: $pink-color;
+						margin-bottom: 20px;
+					}
+					h3 {
+						text-align: center;
+						font-family: Barlow;
+						font-size: 24px;
+						font-style: normal;
+						font-weight: 700;
+						line-height: 31px;
+					}
+				}
+			}
+		} */
+		&__about {
+			display: grid;
+			grid-template-columns: 2fr 1fr;
+			gap: 30px;
+			width: 100%;
+
+			&--left {
+				display: grid;
+				grid-template-columns: 1fr 1fr;
+				gap: 30px;
+				width: 100%;
+				height: 100%;
+
+				.img__section {
+					align-self: center;
+				}
+				.left__image {
+					width: 100%;
+					height: 80%;
+
+					.image {
+						width: 100%;
+						height: 100%;
+						object-fit: fill;
+						border-radius: 10px;
+					}
+				}
+				.right__image {
+					width: 100%;
+					height: 100%;
+					.image {
+						width: 100%;
+						height: 100%;
+						object-fit: fill;
+						border-radius: 10px;
+					}
+				}
+			}
+			&--right {
+				width: 100%;
+				height: 100%;
+				display: flex;
+				align-items: center;
+				.right__container {
+					width: 100%;
+					height: 80%;
+					background-color: $main-color;
+					border-radius: 10px;
+					display: flex;
+					padding: 0 41px;
+					flex-direction: column;
+					align-items: center;
+					justify-content: center;
+					h1 {
+						font-family: Sansita;
+						font-size: 2.5rem;
+						font-style: normal;
+						line-height: 70px;
+						font-weight: 700;
+						margin-bottom: 40px;
+					}
+					p {
+						font-family: Open Sans;
+						font-size: 15px;
+						font-style: normal;
+						font-weight: 400;
+						line-height: 26px;
+					}
+					h5 {
+						padding: 14px 30px;
+						background-color: $pink-color;
+						border-radius: 10px;
+						width: fit-content;
+					}
+				}
+			}
+		}
+	}
+}
+@media screen and (max-width: 1550px) {
+	.body {
+		padding: 0 255px;
+		width: 100%;
+		height: 100%;
+		.section {
+			margin-bottom: 100px;
+		}
+		/* &__title {
+			margin-top: 100px;
+			text-align: left;
+			width: fit-content;
+			p {
+				font-family: Sansita;
+				font-size: 54px;
+				font-style: normal;
+				font-weight: 700;
+				margin-bottom: 10px;
+			}
+			&--option {
+				display: flex;
+				align-items: center;
+				h6 {
+					font-family: Open Sans;
+					font-size: 15px;
+					font-style: normal;
+					line-height: 26px;
+				}
+				.vertical-line {
+					width: 2px;
+					height: 50%;
+					background-color: $text-color;
+					display: flex;
+					margin: 0 15px;
+				}
+			}
+		}
+		&__cta {
+			margin-top: 70px;
+			width: 100%;
+			background: #e5f8fd;
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			&--left {
+				padding: 100px 70px;
+				text-align: left;
+				height: fit-content;
+				.about {
+					border-radius: 5px;
+					background: #e6e6ef;
+					width: fit-content;
+					padding: 7px 15px;
+					color: #ec80a0;
+				}
+				.quote {
+					font-family: Sansita;
+					font-size: 54px;
+					font-style: normal;
+					font-weight: 700;
+					line-height: 70px;
+					margin-bottom: 40px;
+				}
+				.contact {
+					padding: 17px 40px;
+					background-color: #ec80a0;
+					text-align: center;
+					width: fit-content;
+					border-radius: 10px;
+				}
+			}
+			&--right {
+				width: 100%;
+				height: 100%;
+				display: grid;
+				.video {
+					align-self: center;
+					video {
+						width: 100%;
+						height: 100%;
+					}
+				}
+			}
+		}
+		&__proud {
+			&--title {
+				p {
+					font-family: Sansita;
+					font-size: 54px;
+					font-style: normal;
+					font-weight: 700;
+					line-height: 70px;
+					margin-bottom: 50px;
+				}
+			}
+			&--cardGroup {
+				width: 100%;
+				height: 100%;
+				display: grid;
+				grid-template-columns: 1fr 1fr 1fr 1fr;
+				column-gap: 30px;
+				.card {
+					cursor: pointer;
+					border-radius: 10px;
+					box-shadow: 0px 4px 30px 0px rgba(10, 30, 35, 0.1);
+					h2 {
+						font-weight: 700;
+						line-height: 55px;
+						font-family: Sansita;
+						font-size: 42px;
+						color: $pink-color;
+						margin-bottom: 20px;
+					}
+					h3 {
+						text-align: center;
+						font-family: Barlow;
+						font-size: 24px;
+						font-style: normal;
+						font-weight: 700;
+						line-height: 31px;
+					}
+				}
+			}
+		} */
+		&__about {
+			display: grid;
+			grid-template-columns: 2fr 1fr;
+			gap: 30px;
+			width: 100%;
+
+			/*	&--left {
+				display: grid;
+				grid-template-columns: 1fr 1fr;
+				gap: 30px;
+				width: 100%;
+				height: 100%;
+
+				.img__section {
+					align-self: center;
+				}
+				.left__image {
+					width: 100%;
+					height: 80%;
+
+					.image {
+						width: 100%;
+						height: 100%;
+						object-fit: fill;
+						border-radius: 10px;
+					}
+				}
+				.right__image {
+					width: 100%;
+					height: 100%;
+					.image {
+						width: 100%;
+						height: 100%;
+						object-fit: fill;
+						border-radius: 10px;
+					}
+				}
+			} */
+			&--right {
+				width: 100%;
+				height: 100%;
+				display: flex;
+				align-items: center;
+				.right__container {
+					width: 100%;
+					height: 80%;
+					background-color: $main-color;
+					border-radius: 10px;
+					display: flex;
+					padding: 0 20px;
+					flex-direction: column;
+					align-items: center;
+					justify-content: center;
+					h1 {
+						font-family: Sansita;
+						font-size: 2rem;
+						font-style: normal;
+						line-height: 30px;
+						font-weight: 700;
+						margin-bottom: 20px;
+					}
+					p {
+						font-family: Open Sans;
+						font-size: 15px;
+						font-style: normal;
+						font-weight: 400;
+						line-height: 26px;
+						font-size: 1rem;
+					}
+					h5 {
+						font-size: 1.3rem;
+						padding: 5px 10px;
+						background-color: $pink-color;
+						border-radius: 10px;
+						width: fit-content;
+					}
+				}
+			}
+		}
+	}
+}
 </style>
